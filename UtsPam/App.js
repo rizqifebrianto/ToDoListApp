24716bb25997af75a2433c082ca775a30e41662e
Rizqi Fiesta Febrianto <89419086+rizqifebrianto@users.noreply.github.com>
2022-10-18 00:20:19 +0700

    Delete UtsPam directory

--- UtsPam/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';  
-import Main from './component/main'  
-export default class App extends React.Component {  
- render() {  
-  return (  
-    <Main/>  
-  );   
- }  
-}
